fbd17614efcfe0da742fc957f153ad7b6705aad3
Imam Hussain <hussainimam78621@gmail.com>
2023-11-08 17:18:29 +0530

    updated

--- DynamicProgramming/targetsubsetsum.java
+++ DynamicProgramming/targetsubsetsum.java
@@ -18,7 +18,17 @@ public class targetsubsetsum {
                 }
             }
         }
+        printtable(dp);
         return dp[n][sum];
+    }
+    public static void printtable(boolean dp[][]){
+        for(int i=0;i<dp.length;i++){
+           for(int j=0;j<dp[0].length;j++){
+              System.out.print(dp[i][j]+" ");
+           }
+           System.out.println();
+        }
+        System.out.println();
     }
     public static void main(String[] args) {
         int arr[] = {4,2,7,1,3};
