c1438a1370d3c944702d2f300c0afb7456e5e76b
Imam Hussain <hussainimam78621@gmail.com>
2023-11-11 16:05:48 +0530

    new code added

---
+++ DynamicProgramming/LCS/memoizationlcs.java
@@ -0,0 +1,35 @@
+package LCS;
+
+public class memoizationlcs {
+    public static int longestsequence(String str1, String str2,int n,int m,int dp[][]){
+        
+        if(n==0 || m==0){
+            return 0;
+        }
+        if(dp[n][m] != -1){
+            return dp[n][m];
+        }
+        if(str1.charAt(n-1) == str2.charAt(m-1)){
+            return dp[n][m] = longestsequence(str1, str2, n-1, m-1, dp)+ 1;
+        }
+        else{
+            int ans1 = longestsequence(str1, str2, n-1, m, dp);
+            int ans2 = longestsequence(str1, str2, n, m-1, dp);
+            return dp[n][m] = Math.max(ans1, ans2);
+        }
+
+    }
+    public static void main(String[] args) {
+        String  str1 = "ABCDGH";
+        String str2 = "AEDFHR";
+        int n=str1.length();
+        int m=str2.length();
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                dp[i][j]=-1;
+            }
+        }
+        System.out.println(longestsequence(str1, str2, n, m, dp));
+    }
+}
