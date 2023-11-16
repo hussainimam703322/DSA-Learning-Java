c9538c57624fa2f3598cb27cd674c4d08145c549
Imam Hussain <hussainimam78621@gmail.com>
2023-11-16 19:01:45 +0530

    DSA updated

--- DynamicProgramming/knapsack01/minimumPartition.java
+++ DynamicProgramming/knapsack01/minimumPartition.java
@@ -1,5 +1,5 @@
 package knapsack01;
-
+//minimum subset difference
 /*
 Given an array arr of size n containing non-negative integers,
 the task is to divide it into two sets S1 and S2 such that the absolute
@@ -12,7 +12,7 @@ public class minimumPartition {
         for (int i = 0; i < n; i++) {
             sum += arr[i];
         }
-        int W = sum / 2;
+        int W = sum / 2;  //created another bag of capacity w
         int dp[][] = new int[n + 1][W + 1];
 
         for (int i = 1; i < n + 1; i++) {
@@ -36,3 +36,22 @@ public class minimumPartition {
         System.out.println(minipartition(arr));
     }
 }
+/*
+In this problem we have calculated total sum and sum/2 added in two subset set1 and set2
+such that both have same value....
+we do partition in array..
+
+in this only one array so we assume that val[] and wt[] (arr[i-1]) same like in this ways if wt =6 then val=6 if wt = 11 then val =11 so on.....
+
+created two bags one have bag1 capacity sum/2 = w another  bag2 sum2 = sum - sum1(calculated profit) using dp[i][j] (i=n,j=profit or wieght)
+
+if set1 contains {1,11} then set2 must contains{6,5}
+
+if arr contains element less than or = the capacity of bag so that choice in valid 
+if arr contains element more than the capacity of bag so that choice in invalid 
+so element will come in bag based on choices
+
+the we will find the difference of both sum so that difference is minimum...
+
+
+ */
--- DynamicProgramming/minimumJumps.java
+++ DynamicProgramming/minimumJumps.java
@@ -1,29 +1,27 @@
 import java.util.Arrays;
-
 public class minimumJumps {
-    public static int jumps(int arr[]) {
-        int n = arr.length;
+    public static int jumps(int arr[]){
+        int n=arr.length;
         int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-        dp[n - 1] = 0;
+        Arrays.fill(dp,-1);
+        dp[n-1] =0;
 
-        for (int i = n - 2; i >= 0; i--) {
+        for(int i=n-2; i>= 0;i--){
             int steps = arr[i];
             int ans = Integer.MAX_VALUE;
-            for (int j = i + 1; j < n; j++) {
-                if (j <= i + steps && dp[j] != -1) {
-                    ans = Math.min(ans, dp[j]);
+            for(int j=i+1;j<=i+steps && j<n;j++){
+                if(dp[j] != -1){
+                    ans = Math.min(ans,dp[j]+1);
                 }
             }
-            if (ans != Integer.MAX_VALUE) {
-                dp[i] = ans + 1; // Increment the jumps
+            if(ans != Integer.MAX_VALUE){
+                dp[i] = ans;
             }
         }
         return dp[0];
     }
-
     public static void main(String[] args) {
-        int arr[] = {1, 2, 0, 3, 0, 0} ;
+        int arr[] = {2, 3, 1, 1,4};
         System.out.println(jumps(arr));
     }
 }
@@ -36,5 +34,17 @@ Explanation:
 Jump 1 step from first index to
 second index. Then jump 2 steps to reach 
 4th index, and now jump 2 steps to reach
-the end. 
+the end.
+
+SHORT SOLUTION
+static int canReach(int[] A, int N) {
+        // code here
+        int lastReachable = N - 1;
+        for (int i = N - 2; i >= 0; i--) {
+            if (i + A[i] >= lastReachable) {
+                lastReachable = i;
+            }
+        }
+        return (lastReachable == 0) ? 1 : 0;
+    }
  */
