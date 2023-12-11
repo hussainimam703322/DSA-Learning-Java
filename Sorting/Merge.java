bda2ee39a968dd341a427eea3136b4e00b5bef78
Imam Hussain <hussainimam78621@gmail.com>
2023-12-11 22:53:19 +0530

    updated in merge sort

--- Sorting/Merge.java
+++ Sorting/Merge.java
@@ -1,5 +1,48 @@
 public class Merge {
-    static void displayArr(int arr[]){
+    public static void conquer(int arr[], int s, int mid, int e) {
+        int merged[] = new int[e - s + 1];
+        int idx1 = s;
+        int idx2 = mid + 1;
+        int x = 0;
+        while (idx1 <= mid && idx2 <= e) {
+            if (arr[idx1] <= arr[idx2]) {
+                merged[x++] = arr[idx1++];
+            } else {
+                merged[x++] = arr[idx2++];
+            }
+        }
+        while (idx1 <= mid) {
+            merged[x++] = arr[idx1++];
+        }
+        while (idx2 <= e) {
+            merged[x++] = arr[idx2++];
+        }
+        for (int i = 0, j = s; i < merged.length; i++, j++) {
+            arr[j] = merged[i];
+        }
+    }
+
+    public static void divide(int arr[], int s, int e) {
+        if (s < e) {
+            int mid = s + (e - s) / 2;
+            divide(arr, s, mid);
+            divide(arr, mid + 1, e);
+            conquer(arr, s, mid, e);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 5, 1, 3, 8, 7};
+        int n = arr.length;
+        divide(arr, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
+/*
+static void displayArr(int arr[]){
         for(int val : arr){
            System.out.println(val);
         }
@@ -42,12 +85,4 @@ public class Merge {
         merge(arr,l,mid,r);
 
     }
-
-    public static void main(String[] args) {
-        int arr[] = {2,4,5,1,3,8,7};
-        int n = arr.length;
-        mergesort(arr, 0, n-1);
-        displayArr(arr);
-        
-    }
-}
+ */
